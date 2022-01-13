22a903398bdb433d01de8e04ea8330d6c43eff4f
ishachoubisa <isha91choubisa1234@gmail.com>
2022-01-13 11:06:49 +0100

    concepts of legal and illegal identifiers

---
+++ src/main/java/com/learning/javabuildingblocks/LegalOrIllegalIdentifiers.java
@@ -0,0 +1,24 @@
+package com.learning.javabuildingblocks;
+
+public class LegalOrIllegalIdentifiers {
+
+
+
+public static void main(String args[]){
+
+//Legal identifiers
+
+long okidentifier;
+float $OK2Identifier;
+boolean _alsoOKId3ntifi3r;
+char __SStillOkbutKnotsonice$;
+
+//Illegal identifiers
+
+//int 3DPointclass;//identifier cannot begin with number
+//byte hollywood@vine;//@ is not digit,letter, $ or _
+//String *$coffee;//* is not digit,letter,$ or _
+//double public;//public is a reserved word
+//short _;//a single underscore is not allowed
+}
+}
