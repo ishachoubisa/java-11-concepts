73087af59963d6a803c535f62ade35d812f7094b
ishachoubisa <isha91choubisa1234@gmail.com>
2022-01-15 00:38:08 +0100

    concept of compound operator type casting

---
+++ src/main/java/com/learning/operators/CompoundOperator.java
@@ -0,0 +1,21 @@
+package com.learn.operators;
+
+
+public class CompoundOperator {
+
+public static void main(String... args) {
+
+long goat = 5;
+int sheep = 5;
+// sheep = sheep * goat;   // DOES NOT COMPILE
+
+//The compound operator will first cast cat to a long,
+// apply the multiplication of two long values, and then cast the result to an int.
+
+long dog = 10;
+int cat = 5;
+cat *= dog;    // THIS WILL COMPILE
+
+}
+}
+i
