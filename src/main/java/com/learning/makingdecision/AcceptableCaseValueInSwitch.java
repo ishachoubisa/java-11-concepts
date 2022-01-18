0c566d03b52ca87a6a573ebaf0b8f293ead09f47
ishachoubisa <isha91choubisa1234@gmail.com>
2022-01-18 11:14:26 +0100

    commenting for compilation issue

--- src/main/java/com/learning/makingdecision/AcceptableCaseValueInSwitch.java
+++ src/main/java/com/learning/makingdecision/AcceptableCaseValueInSwitch.java
@@ -3,37 +3,37 @@ package com.learning.makingdecision;
 public class AcceptableCaseValueInSwitch {
 
     public static void main(String[] args) {
-
-        feedAnimals();
-        complexExample("firstName", "lastName");
+//
+//        feedAnimals();
+//        complexExample("firstName", "lastName");
     }
 
     private static int complexExample(String firstName, final String
-            lastName) {
-        String middleName = "Patricia";
-        final String suffix = "JR";
+           lastName) {
+//        String middleName = "Patricia";
+//        final String suffix = "JR";
         int id = 0;
         switch (firstName) {
-            case "Test":
-                return 52;
-            case middleName:  // DOES NOT COMPILE, although initialized but expected a final constant
-                id = 5;
-                break;
-            case suffix:
-                id = 0;
-                break;
-            case lastName: //Despite lastName being final, it is not constant as it is passed to the function
-                id = 8;
-                break;
-            //case 5:     // Diff type
-            id = 7;
-            break;
-            //case 'J':   // Diff type
-            id = 10;
-            break;
+//            case "Test":
+//                return 52;
+//            case middleName:  // DOES NOT COMPILE, although initialized but expected a final constant
+//                id = 5;
+//                break;
+//            case suffix:
+//                id = 0;
+//                break;
+//            case lastName: //Despite lastName being final, it is not constant as it is passed to the function
+//                id = 8;
+//                break;
+//            //case 5:     // Diff type
+//            id = 7;
+//            break;
+//            //case 'J':   // Diff type
+//            id = 10;
+//            break;
             //case java.time.DayOfWeek.SUNDAY:  // DOES NOT COMPILE Enum is diff type
-            id = 15;
-            break;
+//            id = 15;
+//            break;
         }
         return id;
     }
