47b0c8f4311d4e99618a219242b5a6784057e59e
ishachoubisa <isha91choubisa1234@gmail.com>
2022-01-18 13:21:32 +0100

    concept of infinite loop

--- src/main/java/com/learning/decisionmaking/InfiniteLoop.java
+++ src/main/java/com/learning/decisionmaking/InfiniteLoop.java
@@ -1,4 +1,24 @@
 package com.learning.decisionmaking;
 
-public class InfiniteLoop {
-}
+    public class InfiniteLoop {
+        public static void main(String[] args) {
+            int height = 1;
+            L1:while(height++<10){
+                System.out.println("height "+ height);
+                long humidity = 12;
+                System.out.println("humidity "+ humidity);
+                L2: do{
+                    if(humidity-- % 12 == 0)  continue L2;
+                    System.out.println("humidity "+ humidity);
+                    int temperature = 30;
+                    L3: for( ; ; ){
+                        temperature++;
+                        if(temperature>50) continue L2;
+                        System.out.println("temperature "+ temperature);
+                    }
+                }while (humidity>4);
+                System.out.println("humidity "+ humidity);
+            }
+        }
+    }
+
