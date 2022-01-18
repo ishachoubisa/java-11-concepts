af5556dabfad44502115b6b507a28a2340dc4fec
ishachoubisa <isha91choubisa1234@gmail.com>
2022-01-18 13:16:35 +0100

    concept of for loop

--- src/main/java/com/learning/decisionmaking/ForLoop.java
+++ src/main/java/com/learning/decisionmaking/ForLoop.java
@@ -1,4 +1,11 @@
 package com.learning.decisionmaking;
 
 public class ForLoop {
+    public static void main(String[] args) {
+        int x=0;
+        for(long y = 0,z=4;x<5&&y<10;x++,y++){
+            System.out.println(y+" ");
+        }
+        System.out.println(x+" ");
+    }
 }
