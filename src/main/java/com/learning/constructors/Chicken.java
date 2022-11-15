ff03aca5e9ec516aa7676da4c655879f092839ab
ishachoubisa <67230593+ishachoubisa@users.noreply.github.com>
2022-11-15 16:55:46 +0100

    Update Chicken.java

--- src/main/java/com/learning/constructors/Chicken.java
+++ src/main/java/com/learning/constructors/Chicken.java
@@ -1,4 +1,13 @@
 package com.learning.constructors;
 
+//Purpose of constructor is to initialize fields
+//another way to initialize fields is to do so directly
+//on line on which they are declare
+//here both approaches are shown
 public class Chicken {
+    int numEggs = 12;//initialize directly on line where declare
+    String name;
+    public Chicken(){
+        name = "Duke";//initialize in constructor
+    }
 }
