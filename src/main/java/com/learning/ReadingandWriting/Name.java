6993f04a1b0990f5feda1ace7b2facbd9b569715
ishachoubisa <67230593+ishachoubisa@users.noreply.github.com>
2022-11-15 16:50:49 +0100

    Update Name.java

--- src/main/java/com/learning/ReadingandWriting/Name.java
+++ src/main/java/com/learning/ReadingandWriting/Name.java
@@ -1,4 +1,11 @@
 package com.learning.ReadingandWriting;
 
 public class Name {
+     String first = "Theodore";
+    String last = "Moose";
+    String full = first + last;
 }
+//Line 4 and 5 both write to fields. Line 6 both reads and writes
+//data. It reads the fields first and last. It then writes the
+//field full.
+
