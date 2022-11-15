de8e9fe9b70ef836d6f38af362da9363ab239577
ishachoubisa <67230593+ishachoubisa@users.noreply.github.com>
2022-11-15 16:54:18 +0100

    Update Example.java

--- src/main/java/com/learning/bitwiseOperator/Example.java
+++ src/main/java/com/learning/bitwiseOperator/Example.java
@@ -1,4 +1,9 @@
 package com.learning.bitwiseOperator;
 
 public class Example {
+  public static void main(String[] args) {
+        int a=3,b=1;
+        int c = a&b;
+        System.out.println("CAT "+ c);
+    }
 }
